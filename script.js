624dea833a3bad9d59560d5a077c69696837c14c
Franpil12 <francopillajo12@gmail.com>
2025-05-14 21:21:37 -0500

    Primera Version

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
