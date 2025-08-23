94e4f2b6e631751390f689ae3058ad8e9c9de1f5
Franco <149031800+Franpil12@users.noreply.github.com>
2025-08-22 22:12:20 -0500

    Cambio 3

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger');
-const nav = document.getElementById('nav-links');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
-});
